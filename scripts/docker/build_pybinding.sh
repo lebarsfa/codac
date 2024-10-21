48cb5eb287dd4ef833e46e82b9f3f771f54a8c24
Simon Rohou <simon.rohou@gmail.com>
2024-10-21 13:27:09 +0200

    Merge branch 'PR' of ssh://github.com/lebarsfa/codac into lebarsfa-PR

--- scripts/docker/build_pybinding.sh
+++ scripts/docker/build_pybinding.sh
@@ -16,8 +16,9 @@ for PYBIN in /opt/python/cp3*/bin; do
   #fi
 
   "${PYBIN}/python" -m pip install --upgrade pip
+  "${PYBIN}/python" -m pip install --upgrade wheel setuptools
   mkdir -p build_dir && cd build_dir
-  cmake -E env CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake -DPYTHON_EXECUTABLE=${PYBIN}/python -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DWITH_CAPD=OFF -DWITH_PYTHON=ON ..
+  cmake -E env CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake -DPYTHON_EXECUTABLE=${PYBIN}/python -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DWITH_CAPD=OFF -DWITH_PYTHON=ON ..
   make -j4
 
   make pip_package
@@ -26,16 +27,15 @@ for PYBIN in /opt/python/cp3*/bin; do
     auditwheel repair "$whl" -w /io/wheelhouse/
   done
   
-  "${PYBIN}/python" -m pip install codac4matlab --no-deps --no-index -f /io/wheelhouse
-  # disabled tests (Matlab branch only):
-  #"${PYBIN}/python" -m pip install numpy --prefer-binary
-  #"${PYBIN}/python" ../examples/02_centered_form/main.py
-  #"${PYBIN}/python" -m unittest discover codac.tests
+  "${PYBIN}/python" -m pip install codac --no-deps --no-index -f /io/wheelhouse
+  "${PYBIN}/python" ../examples/02_centered_form/main.py
+  "${PYBIN}/python" -m pip install numpy --prefer-binary
+  "${PYBIN}/python" -m unittest discover codac.tests
   
-  #make test ARGS="-V --output-on-failure"
-  #echo "start of Testing/Temporary/LastTest.log"
-  #cat Testing/Temporary/LastTest.log
-  #echo "end of Testing/Temporary/LastTest.log"
+  make test ARGS="-V --output-on-failure"
+  echo "start of Testing/Temporary/LastTest.log"
+  cat Testing/Temporary/LastTest.log
+  echo "end of Testing/Temporary/LastTest.log"
 
   cd /io
   rm -fr build_dir
